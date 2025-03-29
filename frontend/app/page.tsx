6a8482520632469eaad0923b4d088d76745dcf7d
VMahluza <mahluzavictor@gmail.com>
2025-03-29 18:01:50 +0200

    Init Frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1>Hello</h1>
+    </div>
+  );
+}
